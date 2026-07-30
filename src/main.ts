26c2bd1119d5239743ef8106d7115465083cb186
Elnur Hajiyev <elnurvl@gmail.com>
2026-07-31 03:36:35 +0400

    fix the CLI runner

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Asset, isSumEqualsToZero } from './asset_improved'
+import { isSumEqualsToZero } from './asset_improved'
 
 function validateInput(val: unknown) {
     if (Number.isNaN(val)) {
@@ -6,7 +6,7 @@ function validateInput(val: unknown) {
     }
 }
 
-const [a, b, c] = process.argv.slice(2).map((arg) => {
+const args = process.argv.slice(2).map((arg) => {
     let r = Number(arg)
     if (Number.isNaN(r)) {
         console.log("You must provide a number.")
@@ -15,9 +15,4 @@ const [a, b, c] = process.argv.slice(2).map((arg) => {
     return r
 })
 
-if (a === undefined || b === undefined || c === undefined) {
-    console.log("You must provide 3 numbers separated by a space.")
-    process.exit(1)
-}
-
-console.log(isSumEqualsToZero(a, b, c))
+console.log(isSumEqualsToZero(...args))
